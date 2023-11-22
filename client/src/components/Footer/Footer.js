672c19469e1c75fd8a87cc1d5981fe393ac6bc94
Wihctoh <rivertoxi911@gmail.com>
2023-11-22 09:37:13 +0300

    add footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,23 +7,78 @@ import food3 from "./assets/food3.png";
 import food4 from "./assets/food4.png";
 
 export default function Footer() {
-    <>
-        <div className={style.footerContainer}>
-            <div className={style.footerDesc}>
-                <div className={style.footerBlock}>
-                    <Image src={footerLogo} alt='titleLogo'></Image>
-                    <h2>Bistro Bliss</h2>
+    const linkFooterBlockLeft = ["Home", "About", "Menu", "Pricing", "Blog", "Contact", "Delivery"];
+    const linkFooterBlockRight = [
+        "Start Here",
+        "Styleguide",
+        "Password Protected",
+        "404 Not Found",
+        "Licenses",
+        "Changelog",
+        "View More",
+    ];
+
+    const foodImg = [
+        { imgPath: food1, imgName: "food1" },
+        { imgPath: food2, imgName: "food2" },
+        { imgPath: food3, imgName: "food3" },
+        { imgPath: food4, imgName: "food4" },
+    ];
+
+    return (
+        <>
+            <div className={style.footerContainer}>
+                <div className={style.footerWrapper}>
+                    <div className={style.footerDesc}>
+                        <div className={style.footerBlock}>
+                            <Image src={footerLogo} alt='titleLogo'></Image>
+                            <h2>Bistro Bliss</h2>
+                        </div>
+
+                        <p>
+                            In the new era of technology we look a in the future with certainty and
+                            pride to for our company and.
+                        </p>
+                    </div>
+
+                    <div className={style.footerNav}>
+                        <div className={style.footerLinkBlockLeft}>
+                            <h2>Pages</h2>
+
+                            {linkFooterBlockLeft.map((el, i) => (
+                                <p key={i}>{el}</p>
+                            ))}
+                        </div>
+
+                        <div className={style.footerLinkBlockRight}>
+                            <h2>Utility Pages</h2>
+
+                            {linkFooterBlockRight.map((el, i) => (
+                                <p key={i}>{el}</p>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className={style.footerFoodBlock}>
+                        <h2>Follow Us On Instagram</h2>
+
+                        <div className={style.footerFood}>
+                            {foodImg.map((el, i) => (
+                                <Image
+                                    className={style.foodImg}
+                                    src={el.imgPath}
+                                    alt={el.imgName}
+                                    key={i}
+                                ></Image>
+                            ))}
+                        </div>
+                    </div>
                 </div>
 
-                <p>
-                    In the new era of technology we look a in the future with certainty and pride to
-                    for our company and.
+                <p className={style.footerAuthor}>
+                    Copyright © 2023 Hashtag Developer. All Rights Reserved
                 </p>
             </div>
-
-            <div className={style.footerNav}></div>
-
-            <div className={style.footerFood}></div>
-        </div>
-    </>;
+        </>
+    );
 }
